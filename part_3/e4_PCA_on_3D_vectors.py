bb24c7951e0b8a8b78a1300089269c508371ef72
Volodymyr Donets <vol.donets@gmail.com>
2022-12-04 15:57:56 +0200

    PCA vs SVD vs LDiA

--- part_3/e4_PCA_on_3D_vectors.py
+++ part_3/e4_PCA_on_3D_vectors.py
@@ -4,11 +4,25 @@ from sklearn.decomposition import PCA
 import seaborn
 import matplotlib.pyplot as plt
 from nlpia.data.loaders import get_data
+from sklearn.decomposition import TruncatedSVD
+from sklearn.decomposition import LatentDirichletAllocation
+from mlxtend.preprocessing import minmax_scaling
 
 
 df = get_data('pointcloud').sample(1000)
 pca = PCA(n_components=2)
 df2d = pd.DataFrame(pca.fit_transform(df), columns=list('xy'))
-df2d.plot(kind='scatter', x='x', y='y')
+df2d.plot(kind='scatter', x='x', y='y', title='Horse via PCA')
 plt.show()
 
+
+svd = TruncatedSVD(n_components=2, n_iter=10)
+df2d = pd.DataFrame(svd.fit_transform(df), columns=list('xy'))
+df2d.plot(kind='scatter', x='x', y='y', title='Horse via SVD')
+plt.show()
+
+ldia = LatentDirichletAllocation(n_components=2)
+df = minmax_scaling(df, columns=list('xyz'), min_val=-1, max_val=1)
+df2d = pd.DataFrame(ldia.fit_transform(df), columns=list('xy'))
+df2d.plot(kind='scatter', x='x', y='y', title='Horse via LDiA')
+plt.show()
