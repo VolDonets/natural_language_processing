13c78a33dac724d98e490db7d5e0eb6ed3d439f8
Volodymyr Donets <vol.donets@gmail.com>
2022-12-11 13:30:15 +0200

    I've tried load fasttext model for Ukrainian language, but it took too much memory and gensim methods didn't work.

--- part_5/e7_fastText.py
+++ part_5/e7_fastText.py
@@ -1,7 +1,7 @@
 from gensim.models.fasttext import FastText
 
 # download the bin + text model for your language of choice
-MODEL_PATH = ""
+MODEL_PATH = "../src/ukr_fastText/cc.uk.300.bin.gz"
 ft_model = FastText.load_fasttext_format(model_file=MODEL_PATH)
-print(ft_model.most_similar('soccer'))
+print(ft_model.most_similar(positive=['сало'], topn=5))
 
